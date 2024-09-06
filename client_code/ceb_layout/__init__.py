08a7bf3ba6acf8383ac350eb39408034e13d4d8b
cebaraldi@gmail.com <cebaraldi@gmail.com>
2024-09-06 06:03:23 +0000

    Created ceb_layout; Edited settings; Deleted Form1

--- client_code/ceb_layout/__init__.py
+++ client_code/ceb_layout/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import ceb_layoutTemplate
 from anvil import *
 
 
-class Form1(Form1Template):
+class ceb_layout(ceb_layoutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
